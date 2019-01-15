785ec9eaa880e114d92038a993378c6ca61caf65
unknown <johnley00@gmail.com>
2019-01-16 01:26:52 +0800

    Update

--- app/src/main/java/com/robert/soapbox/View/MainActivity.java
+++ app/src/main/java/com/robert/soapbox/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.robert.soapbox;
+package com.robert.soapbox.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.robert.soapbox.R;
 
 public class MainActivity extends AppCompatActivity {
 
